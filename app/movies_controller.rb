944b6a20aebbce19851b078f06f2250f6f528ed4
jasonemarkson <jasonmarkson76@gmail.com>
2020-11-03 11:07:37 -0500

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -10,7 +10,7 @@ require 'pry'
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
-  movie
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
